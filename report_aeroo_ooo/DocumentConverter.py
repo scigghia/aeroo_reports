b0fe7debaaff8e91f92e7193e5a067e07f7906d4
scigghia <info@andreacometa.it>
2016-12-29 16:02:29 +0100

    test branch

---
+++ report_aeroo_ooo/DocumentConverter.py
@@ -0,0 +1,11 @@
+print "carne"
+
+class DocumentConverter:
+
+    def __init__(self, host='localhost', port='', ooo_restart_cmd=None):
+        self._host = host
+        self._port = port
+
+class DocumentConversionException:
+    def __init__(self):
+        print "carnazza"
